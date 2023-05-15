af7752e76f9f138535bfa9637171f9e0f4f48352
Afonso Roberto Plantes <afonso.plants@gmail.com>
2023-05-15 12:44:11 -0400

    Testes

---
+++ powerFlow/classNewton.py
@@ -0,0 +1,27 @@
+import cmath as cmt
+import math as mt
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class Newton():
+    def __init__(self) -> None:
+        pass
+    
+    def busBar(self, barra, code, tensao, ang, carga, geracao):
+        
+        """
+        barra -> numero sequencia da barra
+        code: 1 -> Barra tensão e angulo
+              2 -> Barra P e Q
+              3 -> Barra P e V        
+        angulo -> graus
+        tensão -> pu
+        carga e geracao -> VA
+
+
+
+
+        """
+    
